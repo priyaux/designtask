d1754ef8395901fe512021bb6db378cec312d047
priya-pravashni <44159257+priya-pravashni@users.noreply.github.com>
2018-10-18 08:07:31 +1100

    animate change

--- js/form-validation.js
+++ js/form-validation.js
@@ -2,15 +2,23 @@
   'use strict';
   $.validator.setDefaults({
     submitHandler: function() {
-      alert("submitted!");
+      d = $('#domain').val();
+	  u = $('#username').val();
+	  p = $('#password').val();
+	  if(d == "PBS" && u == "smithjo" && p == "12345678"){
+		 window.location.href = "pin.html";
+	  }
+	  else {
+		  
+	  }
     }
   });
   $(function() {
     // validate the comment form when it is submitted
     $("#commentForm").validate({
-      errorPlacement: function(label, element) {
-        label.addClass('mt-2 text-danger');
-        label.insertAfter(element);
+      errorPlacement: function(p, element) {
+        p.addClass('mt-2 text-danger');
+        p.insertAfter(element);
       },
       highlight: function(element, errorClass) {
         $(element).parent().addClass('has-danger')
@@ -20,54 +28,32 @@
     // validate signup form on keyup and submit
     $("#signupForm").validate({
       rules: {
-        firstname: "required",
-        lastname: "required",
-        username: {
+        domain: "required",
+        username: "required",
+        password: {
           required: true,
-          minlength: 2
+          minlength: 8
         },
         password: {
           required: true,
-          minlength: 5
-        },
-        confirm_password: {
-          required: true,
-          minlength: 5,
-          equalTo: "#password"
-        },
-        email: {
-          required: true,
-          email: true
-        },
-        topic: {
-          required: "#newsletter:checked",
-          minlength: 2
-        },
-        agree: "required"
+          minlength: 8
+        }
       },
       messages: {
-        firstname: "Please enter your firstname",
-        lastname: "Please enter your lastname",
+        domain: "Please enter your Domain",
+        username: "Please enter your username",
         username: {
-          required: "Please enter a username",
-          minlength: "Your username must consist of at least 2 characters"
+          required: "Please enter a username"
+          //minlength: "Your username must consist of at least 2 characters"
         },
         password: {
           required: "Please provide a password",
-          minlength: "Your password must be at least 5 characters long"
-        },
-        confirm_password: {
-          required: "Please provide a password",
-          minlength: "Your password must be at least 5 characters long",
-          equalTo: "Please enter the same password as above"
-        },
-        email: "Please enter a valid email address",
-        agree: "Please accept our policy",
-        topic: "Please select at least 2 topics"
+          minlength: "Your password must be at least 8 characters long"
+        }
       },
-      errorPlacement: function(label, element) {
-        label.addClass('mt-2 text-danger');
-        label.insertAfter(element);
+      errorPlacement: function(p, element) {
+        p.addClass('mt-2 text-danger');
+        p.insertAfter(element);
       },
       highlight: function(element, errorClass) {
         $(element).parent().addClass('has-danger')
